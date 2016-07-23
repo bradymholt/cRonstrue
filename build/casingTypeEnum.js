c8033c576b6eb234d20843846b59265d7cf9595f
Brady Holt <brady.holt@gmail.com>
2016-07-23 17:24:14 -0500

    Remove files from /build

--- build/casingTypeEnum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var CasingTypeEnum;
-(function (CasingTypeEnum) {
-    CasingTypeEnum[CasingTypeEnum["TITLE"] = 0] = "TITLE";
-    CasingTypeEnum[CasingTypeEnum["SENTENCE"] = 1] = "SENTENCE";
-    CasingTypeEnum[CasingTypeEnum["LOWERCASE"] = 2] = "LOWERCASE";
-})(CasingTypeEnum || (CasingTypeEnum = {}));
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = CasingTypeEnum;
