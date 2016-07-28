b8e8ab8cc2153e653ef68e607950f974831b61c4
Brady Holt <brady.holt@gmail.com>
2016-07-28 11:36:00 -0500

    Rename test to cronstrue.js

--- test/cronstrue.js
+++ test/cronstrue.js
@@ -1,6 +1,6 @@
 "use strict";
 var assert = require('chai').assert;
-var construe = require('../build/expressionDescriptor');
+var construe = require('../build/cronstrue');
 
 describe('ExpressionDescriptor', function () {
     describe('every', function () {
