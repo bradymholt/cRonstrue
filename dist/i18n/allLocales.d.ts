fe56f034356aa9fe7873c397376762df045d08a1
Brady Holt <brady.holt@gmail.com>
2017-03-30 09:53:10 -0500

    Build latest /dist

---
+++ dist/i18n/allLocales.d.ts
@@ -0,0 +1,15 @@
+export { en } from "./locales/en";
+export { de } from "./locales/de";
+export { es } from "./locales/es";
+export { fr } from "./locales/fr";
+export { it } from "./locales/it";
+export { nl } from "./locales/nl";
+export { nb } from "./locales/nb";
+export { sv } from "./locales/sv";
+export { pl } from "./locales/pl";
+export { pt_BR } from "./locales/pt_BR";
+export { ro } from "./locales/ro";
+export { ru } from "./locales/ru";
+export { tr } from "./locales/tr";
+export { uk } from "./locales/uk";
+export { zh_CN } from "./locales/zh_CN";
---
+++ dist/stringUtilities.d.ts
@@ -0,0 +1,4 @@
+export declare class StringUtilities {
+    static format(template: string, ...values: string[]): string;
+    static containsAny(text: string, searchStrings: string[]): boolean;
+}
