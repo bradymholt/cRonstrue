a72fd0050c4b1ee9ad29275f17e8ca2982a4454d
Brady Holt <brady.holt@gmail.com>
2017-04-03 16:17:36 -0500

    Use package.json for settings; restore demo

--- README.md
+++ README.md
@@ -65,7 +65,7 @@ For more usage examples, including a demonstration of how cRonstrue can handle s
 
 ## Demo
 
-A demo is available here: [http://bradyholt.github.io/cRonstrue/](http://bradyholt.github.io/cRonstrue/)
+A demo is available here [here](http://bradyholt.github.io/cRonstrue/#cronstrue-demo).
 
 ## i18n
 
--- docs/generate.js
+++ docs/generate.js
@@ -3,18 +3,21 @@
 
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
-const handlebars = require("handlebars");
+const fs = require('fs');
+const path = require('path');
+const handlebars = require('handlebars');
 const marked = require('marked');
+
+let templateFile = path.join(__dirname, 'template.hbs.html');
+let outputFile = path.join(__dirname, 'index.html');
+
+let config = require('../package.json');
 
 let readme = fs.readFileSync(process.argv[2], 'utf8');
 let content = marked(readme);
 
-const templateFile = path.join(__dirname, 'template.hbs.html');
-const outputFile = path.join(__dirname, 'index.html');
 let templateSource = fs.readFileSync(templateFile, 'utf8');
 let template = handlebars.compile(templateSource);
-let html = template({ content: content });
+let html = template({ config: config, content: content });
 
 fs.writeFileSync(outputFile, html, {encoding: 'utf8' });
