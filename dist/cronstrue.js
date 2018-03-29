bec3dae8e3eb7676f16c0f9671237431e824e330
Brady Holt <brady.holt@gmail.com>
2018-03-29 17:26:55 -0500

    Build latest /dist

--- dist/cronstrue.js
+++ dist/cronstrue.js
@@ -2,7 +2,7 @@
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
-		define("cronstrue", [], factory);
+		define([], factory);
 	else if(typeof exports === 'object')
 		exports["cronstrue"] = factory();
 	else
