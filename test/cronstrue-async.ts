fccae3df0922e09ffc94d1dda62b31149c751e0d
Severin <sevefriede@gmail.com>
2018-06-20 21:58:26 +0200

    updated unit tests and fixed problems with partial override

--- test/cronstrue-async.ts
+++ test/cronstrue-async.ts
@@ -12,39 +12,50 @@ describe("cronstrueAsync", () => {
     });
   });
 
-  // describe("de", () => {
-  //   it("* * * * *", async () => {
-  //     const cronstrue = await cronstrueAsync.initialize(["de"]);
-  //     assert.equal(cronstrue.toString("* * * * *", { locale: "de" }), "Jede Minute");
-  //   });
+  describe("de", () => {
+    it("* * * * *", async () => {
+      const cronstrue = await cronstrueAsync.initialize([{ languageCode: "de" }]);
+      assert.equal(cronstrue.toString("* * * * *", { locale: "de" }), "Jede Minute");
+    });
 
-  //   it("*/5 15 * * MON-FRI", async () => {
-  //     const cronstrue = await cronstrueAsync.initialize(["de"]);
-  //     assert.equal(
-  //       cronstrue.toString("*/5 15 * * MON-FRI", { locale: "de" }),
-  //       "Alle 5 Minuten, um 15:00, Montag bis Freitag"
-  //     );
-  //   });
-  // });
+    it("*/5 15 * * MON-FRI", async () => {
+      const cronstrue = await cronstrueAsync.initialize([{ languageCode: "de" }]);
+      assert.equal(
+        cronstrue.toString("*/5 15 * * MON-FRI", { locale: "de" }),
+        "Alle 5 Minuten, um 15:00, Montag bis Freitag"
+      );
+    });
+  });
 
-  // describe("de & es", () => {
-  //   it("* * * * *", async () => {
-  //     const cronstrue = await cronstrueAsync.initialize(["de", "es"]);
-  //     assert.equal(cronstrue.toString("* * * * *", { locale: "de" }), "Jede Minute");
-  //     assert.equal(cronstrue.toString("* * * * *", { locale: "es" }), "Cada minuto");
-  //   });
+  describe("de & es", () => {
+    it("* * * * *", async () => {
+      const cronstrue = await cronstrueAsync.initialize([{ languageCode: "de" }, { languageCode: "es" }]);
+      assert.equal(cronstrue.toString("* * * * *", { locale: "de" }), "Jede Minute");
+      assert.equal(cronstrue.toString("* * * * *", { locale: "es" }), "Cada minuto");
+    });
 
-  //   it("*/5 15 * * MON-FRI", async () => {
-  //     const cronstrue = await cronstrueAsync.initialize(["de", "es"]);
-  //     assert.equal(
-  //       cronstrue.toString("*/5 15 * * MON-FRI", { locale: "de" }),
-  //       "Alle 5 Minuten, um 15:00, Montag bis Freitag"
-  //     );
-  //     assert.equal(
-  //       cronstrue.toString("*/5 15 * * MON-FRI", { locale: "es" }),
-  //       "Cada 5 minutos, a las 03:00 PM, de lunes a viernes"
-  //     );
-  //   });
-  // });
+    it("*/5 15 * * MON-FRI", async () => {
+      const cronstrue = await cronstrueAsync.initialize([{ languageCode: "de" }, { languageCode: "es" }]);
+      assert.equal(
+        cronstrue.toString("*/5 15 * * MON-FRI", { locale: "de" }),
+        "Alle 5 Minuten, um 15:00, Montag bis Freitag"
+      );
+      assert.equal(
+        cronstrue.toString("*/5 15 * * MON-FRI", { locale: "es" }),
+        "Cada 5 minutos, a las 03:00 PM, de lunes a viernes"
+      );
+    });
+  });
+
+  describe("locale override", () => {
+    it("should return the overriden value", async () => {
+      const cronstrue = await cronstrueAsync.initialize([{ languageCode: "de", localeOverride: { everyMinute: () => "Foo Bar" } }]);
+      assert.equal(cronstrue.toString("* * * * *", { locale: "de" }), "Foo Bar");
+      assert.equal(
+        cronstrue.toString("*/5 15 * * MON-FRI", { locale: "de" }),
+        "Alle 5 Minuten, um 15:00, Montag bis Freitag"
+      );
+    });
+  });
 
 });
