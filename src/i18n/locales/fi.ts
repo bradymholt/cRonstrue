c62bf5a86d527355ed3c8b15207831553beaa26b
Marcos Duque <mduqueoviedo@users.noreply.github.com>
2020-02-20 15:20:31 +0100

    Update Finnish locale
    A couple more of Finnish changes (just missing spaces)

--- src/i18n/locales/fi.ts
+++ src/i18n/locales/fi.ts
@@ -11,7 +11,7 @@ export class fi implements Locale {
     return "Klo";
   }
   atSpace(): string {
-    return "Klo";
+    return "Klo ";
   }
   atX0(): string {
     return "klo %s";
@@ -134,7 +134,7 @@ export class fi implements Locale {
     return " ja ";
   }
   spaceX0OfTheMonth(): string {
-    return "%s kuukaudessa";
+    return " %s kuukaudessa";
   }
   third(): string {
     return "kolmas";
