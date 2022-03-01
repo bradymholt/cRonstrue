681b371182829b62d869b5410784c59dcf31a0a2
Daniel Cohen Gindi <danielgindi@gmail.com>
2022-03-01 11:35:08 +0200

    Removed dist from git, only create on `prepublish` to include in npm package

--- dist/i18n/allLocalesLoader.d.ts
+++
@@ -1,6 +0,0 @@
-import { Locale } from "./locale";
-export declare class allLocalesLoader {
-    load(availableLocales: {
-        [name: string]: Locale;
-    }): void;
-}
--- dist/i18n/localeLoader.d.ts
+++
@@ -1,6 +0,0 @@
-import { Locale } from "./locale";
-export interface LocaleLoader {
-    load(availableLocales: {
-        [name: string]: Locale;
-    }): void;
-}
