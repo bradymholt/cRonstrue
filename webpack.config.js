ab0c7ab8a0d8f1c46074b17cdd34fd99dd6f7ab0
Daniel Cohen Gindi <danielgindi@gmail.com>
2022-03-01 11:34:55 +0200

    Compile separate locales to allow hand-picking

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,94 @@
 var webpack = require("webpack");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserJsPlugin = require('terser-webpack-plugin');
 var libraryName = require("./package.json").name;
 var withLocalesSuffix = "-i18n";
+var fs = require('fs');
+var path = require('path');
 
-module.exports = {
-  mode: "production",
-  entry: {
-    [libraryName]: "./src/cronstrue.ts",
-    [libraryName + ".min"]: "./src/cronstrue.ts",
-    [libraryName + withLocalesSuffix]: "./src/cronstrue-i18n.ts",
-    [libraryName + withLocalesSuffix + ".min"]: "./src/cronstrue-i18n.ts"
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name].js",
-    library: libraryName,
-    libraryTarget: "umd",
-    umdNamedDefine: true,
-    // Workaround for webpack 4 umd bug (Ref: https://github.com/webpack/webpack/issues/6522)
-    globalObject: "typeof self !== 'undefined' ? self : this"
-  },
-  resolve: {
-    extensions: [".js", ".ts"]
-  },
-  module: {
-    /*
-         * Each loader needs an associated Regex test that goes through each
-         * of the files you've included (or in this case, all files but the
-         * ones in the excluded directories) and finds all files that pass
-         * the test. Then it will apply the loader to that file. I haven't
-         * installed ts-loader yet, but will do that shortly.
-         */
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: "ts-loader"
-      }
-    ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new UglifyJsPlugin({
-        include: /\.min\.js$/
-      })
-    ]
-  }
+var entryPoints = {
+  [libraryName]: "./src/cronstrue.ts",
+  [libraryName + ".min"]: "./src/cronstrue.ts",
+  [libraryName + withLocalesSuffix]: "./src/cronstrue-i18n.ts",
+  [libraryName + withLocalesSuffix + ".min"]: "./src/cronstrue-i18n.ts"
 };
+
+var localeEntryPoints = {};
+for (let locale of fs.readdirSync("./src/i18n/locales")) {
+    const code = path.basename(locale, path.extname(locale));
+    localeEntryPoints["locales/" + code] = "./src/i18n/locales/" + locale;
+}
+
+module.exports = [
+    {
+      mode: "production",
+      entry: entryPoints,
+      output: {
+        path: __dirname + "/dist",
+        filename: "[name].js",
+        library: libraryName,
+        libraryTarget: "umd",
+        umdNamedDefine: true,
+      },
+      resolve: {
+        extensions: [".js", ".ts"]
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts$/,
+            loader: "ts-loader"
+          }
+        ]
+      },
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserJsPlugin({
+            include: /\.min\.js$|i18n\/locales/
+          })
+        ]
+      }
+    },
+    {
+      mode: "production",
+      entry: localeEntryPoints,
+      output: {
+        path: __dirname,
+        filename: "[name].js",
+        library: '[name]',
+        libraryTarget: "umd",
+        umdNamedDefine: true,
+      },
+      resolve: {
+        extensions: [".js", ".ts"]
+      },
+      externals: {
+        cronstrue: "cronstrue",
+      },
+      module: {
+        rules: [
+          {
+            test: /i18n[\/\\]locales/,
+            loader: "custom-loader",
+            options: {
+              process: function (resourcePath, source) {
+                let localeCode = resourcePath.match(/i18n[\/\\]locales[\/\\]([a-zA-Z_]+)\.ts$/)[1];
+                source = "import Cronstrue from \"cronstrue\";" + 
+                    source + `\nCronstrue.locales["${localeCode}"] = new ${localeCode}();`;
+                return source;
+              }
+            }
+          },
+          {
+            test: /\.ts$/,
+            loader: "ts-loader"
+          }
+        ]
+      },
+      resolveLoader: {
+        alias: {
+          "custom-loader": path.resolve(__dirname, "scripts/custom-loader"),
+        },
+      }
+    }
+];
