b09e9a629363503563a5101d1af0960d55041efd
dalex-am <60896585+dalex-am@users.noreply.github.com>
2022-11-11 17:52:21 +0300

    fix optional types and undefined string (#264)
    * fix optional types and undefined string
    
    * fix locale DOW = 0
    
    Co-authored-by: Александр Долганов <a.dolganov@infomaximum-llc.ru>

--- src/i18n/locales/ru.ts
+++ src/i18n/locales/ru.ts
@@ -2,15 +2,17 @@
 
 import { Locale } from "../locale";
 
-const getPhraseByNumber = (str: string, words: Array<string>) => {
+const getPhraseByNumber = (str: string | undefined, words: Array<string>) => {
   const number = Number(str);
-  return number
+  return number !== undefined
     ? words[number % 100 > 4 && number % 100 < 20 ? 2 : [2, 0, 1, 1, 1, 2][number % 10 < 5 ? Math.abs(number) % 10 : 5]]
     : words[2];
 };
-const getPhraseByDayOfWeek = (str: string, words: Array<string>) => {
+const getPhraseByDayOfWeek = (str: string | undefined, words: Array<string>) => {
   const number = Number(str);
-  return number ? words[number === 0 ? 0 : number === 1 || number === 2 || number === 4 ? 1 : 2] : words[1];
+  return number !== undefined
+    ? words[number === 0 ? 0 : number === 1 || number === 2 || number === 4 ? 1 : 2]
+    : words[1];
 };
 
 export class ru implements Locale {
@@ -53,25 +55,25 @@ export class ru implements Locale {
   everySecond() {
     return "каждую секунду";
   }
-  everyX0Seconds(s: string) {
+  everyX0Seconds(s?: string) {
     return getPhraseByNumber(s, ["каждую %s секунду", "каждые %s секунды", "каждые %s секунд"]);
   }
   secondsX0ThroughX1PastTheMinute() {
     return "секунды с %s по %s";
   }
-  atX0SecondsPastTheMinute(s: string) {
+  atX0SecondsPastTheMinute(s?: string) {
     return getPhraseByNumber(s, ["в %s секунду", "в %s секунды", "в %s секунд"]);
   }
-  everyX0Minutes(s: string) {
+  everyX0Minutes(s?: string) {
     return getPhraseByNumber(s, ["каждую %s минуту", "каждые %s минуты", "каждые %s минут"]);
   }
   minutesX0ThroughX1PastTheHour() {
     return "минуты с %s по %s";
   }
-  atX0MinutesPastTheHour(s: string) {
+  atX0MinutesPastTheHour(s?: string) {
     return getPhraseByNumber(s, ["в %s минуту", "в %s минуты", "в %s минут"]);
   }
-  everyX0Hours(s: string) {
+  everyX0Hours(s?: string) {
     return getPhraseByNumber(s, ["каждый %s час", "каждые %s часа", "каждые %s часов"]);
   }
   betweenX0AndX1() {
@@ -83,31 +85,31 @@ export class ru implements Locale {
   commaEveryDay() {
     return ", каждый день";
   }
-  commaEveryX0DaysOfTheWeek(s: string) {
+  commaEveryX0DaysOfTheWeek(s?: string) {
     return getPhraseByNumber(s, ["", ", каждые %s дня недели", ", каждые %s дней недели"]);
   }
-  commaX0ThroughX1(s: string) {
-    return s[0] == "2" || s[0] == "3" ? ", со %s по %s" : ", с %s по %s";
+  commaX0ThroughX1(s?: string) {
+    return s && (s[0] == "2" || s[0] == "3") ? ", со %s по %s" : ", с %s по %s";
   }
-  commaAndX0ThroughX1(s: string) {
-    return s[0] == "2" || s[0] == "3" ? " и со %s по %s" : " и с %s по %s";
+  commaAndX0ThroughX1(s?: string) {
+    return s && (s[0] == "2" || s[0] == "3") ? " и со %s по %s" : " и с %s по %s";
   }
-  first(s: string) {
+  first(s?: string) {
     return getPhraseByDayOfWeek(s, ["первое", "первый", "первую"]);
   }
-  second(s: string) {
+  second(s?: string) {
     return getPhraseByDayOfWeek(s, ["второе", "второй", "вторую"]);
   }
-  third(s: string) {
+  third(s?: string) {
     return getPhraseByDayOfWeek(s, ["третье", "третий", "третью"]);
   }
-  fourth(s: string) {
+  fourth(s?: string) {
     return getPhraseByDayOfWeek(s, ["четвертое", "четвертый", "четвертую"]);
   }
-  fifth(s: string) {
+  fifth(s?: string) {
     return getPhraseByDayOfWeek(s, ["пятое", "пятый", "пятую"]);
   }
-  commaOnThe(s: string) {
+  commaOnThe(s?: string) {
     return s === "2" ? ", во " : ", в ";
   }
   spaceX0OfTheMonth() {
@@ -116,16 +118,16 @@ export class ru implements Locale {
   lastDay() {
     return "последний день";
   }
-  commaOnTheLastX0OfTheMonth(s: string) {
+  commaOnTheLastX0OfTheMonth(s?: string) {
     return getPhraseByDayOfWeek(s, [", в последнее %s месяца", ", в последний %s месяца", ", в последнюю %s месяца"]);
   }
-  commaOnlyOnX0(s: string) {
-    return s[0] === "2" ? ", только во %s" : ", только в %s";
+  commaOnlyOnX0(s?: string) {
+    return s && s[0] === "2" ? ", только во %s" : ", только в %s";
   }
   commaAndOnX0() {
     return ", и %s";
   }
-  commaEveryX0Months(s: string) {
+  commaEveryX0Months(s?: string) {
     return getPhraseByNumber(s, ["", " каждые %s месяца", " каждые %s месяцев"]);
   }
   commaOnlyInMonthX0() {
@@ -140,7 +142,7 @@ export class ru implements Locale {
   commaOnTheLastWeekdayOfTheMonth() {
     return ", в последний будний день месяца";
   }
-  commaDaysBeforeTheLastDayOfTheMonth(s: string) {
+  commaDaysBeforeTheLastDayOfTheMonth(s?: string) {
     return getPhraseByNumber(s, [
       ", за %s день до конца месяца",
       ", за %s дня до конца месяца",
@@ -156,16 +158,16 @@ export class ru implements Locale {
   commaOnTheX0OfTheMonth() {
     return ", в %s месяца";
   }
-  commaEveryX0Days(s: string) {
+  commaEveryX0Days(s?: string) {
     return getPhraseByNumber(s, [", каждый %s день", ", каждые %s дня", ", каждые %s дней"]);
   }
-  commaBetweenDayX0AndX1OfTheMonth(s: string) {
-    return s.substring(0, s.indexOf("-")) == "2" ? ", со %s по %s число месяца" : ", с %s по %s число месяца";
+  commaBetweenDayX0AndX1OfTheMonth(s?: string) {
+    return s && s.substring(0, s.indexOf("-")) == "2" ? ", со %s по %s число месяца" : ", с %s по %s число месяца";
   }
-  commaOnDayX0OfTheMonth(s: string) {
-    return s[0] == "2" ? ", во %s число месяца" : ", в %s число месяца";
+  commaOnDayX0OfTheMonth(s?: string) {
+    return s && s[0] == "2" ? ", во %s число месяца" : ", в %s число месяца";
   }
-  commaEveryX0Years(s: string) {
+  commaEveryX0Years(s?: string) {
     return getPhraseByNumber(s, [", каждый %s год", ", каждые %s года", ", каждые %s лет"]);
   }
   commaStartingX0() {
@@ -195,7 +197,7 @@ export class ru implements Locale {
       "декабрь",
     ];
   }
-  monthsOfTheYearInCase(f: number) {
+  monthsOfTheYearInCase(f?: number) {
     return f == 1
       ? [
           "января",
