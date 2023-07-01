89aa3736022d7fd1341d7f1d343065d30a154379
Nguyen Tan Phap <76060868+rikkapro0128@users.noreply.github.com>
2023-07-01 10:52:07 +0700

    add translate vietnamese

---
+++ src/i18n/locales/vi.ts
@@ -0,0 +1,179 @@
+// English
+
+import { Locale } from "../locale";
+export class vi implements Locale {
+  atX0SecondsPastTheMinuteGt20(): string|null {
+    return null;
+  }
+  atX0MinutesPastTheHourGt20(): string|null {
+    return null;
+  }
+  commaMonthX0ThroughMonthX1(): string|null {
+    return null;
+  }
+  commaYearX0ThroughYearX1(): string|null {
+    return null;
+  }
+
+  use24HourTimeFormatByDefault() {
+    return false;
+  }
+
+  anErrorOccuredWhenGeneratingTheExpressionD() {
+    return "Đã xảy ra lỗi khi tạo mô tả biểu thức. Vui lòng kiểm tra cú pháp biểu thức cron.";
+  }
+  everyMinute() {
+    return "mỗi phút";
+  }
+  everyHour() {
+    return "mỗi giờ";
+  }
+  atSpace() {
+    return "Vào ";
+  }
+  everyMinuteBetweenX0AndX1() {
+    return "Mỗi phút giữa %s and %s";
+  }
+  at() {
+    return "Vào";
+  }
+  spaceAnd() {
+    return " và";
+  }
+  everySecond() {
+    return "mỗi giây";
+  }
+  everyX0Seconds() {
+    return "mỗi %s giây";
+  }
+  secondsX0ThroughX1PastTheMinute() {
+    return "giây thứ %s qua phút thứ %s";
+  }
+  atX0SecondsPastTheMinute() {
+    return "tại giây thứ %s của mỗi phút";
+  }
+  everyX0Minutes() {
+    return "mỗi %s phút";
+  }
+  minutesX0ThroughX1PastTheHour() {
+    return "phút thứ %s qua %s tiếng";
+  }
+  atX0MinutesPastTheHour() {
+    return "vào %s phút của mỗi tiếng";
+  }
+  everyX0Hours() {
+    return "mỗi %s tiếng";
+  }
+  betweenX0AndX1() {
+    return "giữa %s và %s";
+  }
+  atX0() {
+    return "vào %s";
+  }
+  commaEveryDay() {
+    return ", mỗi ngày";
+  }
+  commaEveryX0DaysOfTheWeek() {
+    return ", mỗi %s ngày trong tuần";
+  }
+  commaX0ThroughX1() {
+    return ", %s đến %s";
+  }
+  commaAndX0ThroughX1() {
+    return ", %s đến %s";
+  }
+  first() {
+    return "first";
+  }
+  second() {
+    return "second";
+  }
+  third() {
+    return "third";
+  }
+  fourth() {
+    return "fourth";
+  }
+  fifth() {
+    return "fifth";
+  }
+  commaOnThe() {
+    return ", on the ";
+  }
+  spaceX0OfTheMonth() {
+    return " %s of the month";
+  }
+  lastDay() {
+    return "the last day";
+  }
+  commaOnTheLastX0OfTheMonth() {
+    return ", on the last %s of the month";
+  }
+  commaOnlyOnX0() {
+    return ", only on %s";
+  }
+  commaAndOnX0() {
+    return ", and on %s";
+  }
+  commaEveryX0Months() {
+    return ", every %s months";
+  }
+  commaOnlyInX0() {
+    return ", only in %s";
+  }
+  commaOnTheLastDayOfTheMonth() {
+    return ", on the last day of the month";
+  }
+  commaOnTheLastWeekdayOfTheMonth() {
+    return ", on the last weekday of the month";
+  }
+  commaDaysBeforeTheLastDayOfTheMonth() {
+    return ", %s days before the last day of the month";
+  }
+  firstWeekday() {
+    return "first weekday";
+  }
+  weekdayNearestDayX0() {
+    return "weekday nearest day %s";
+  }
+  commaOnTheX0OfTheMonth() {
+    return ", on the %s of the month";
+  }
+  commaEveryX0Days() {
+    return ", mỗi %s ngày";
+  }
+  commaBetweenDayX0AndX1OfTheMonth() {
+    return ", giữa ngày %s và %s trong tháng";
+  }
+  commaOnDayX0OfTheMonth() {
+    return ", vào %s ngày trong tháng";
+  }
+  commaEveryHour() {
+    return ", mỗi tiếng";
+  }
+  commaEveryX0Years() {
+    return ", mỗi %s năm";
+  }
+  commaStartingX0() {
+    return ", bắt đầu %s";
+  }
+  daysOfTheWeek() {
+    return ["Chủ nhật", "Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7"];
+  }
+  monthsOfTheYear() {
+    return [
+      "Tháng 1",
+      "Tháng 2",
+      "Tháng 3",
+      "Tháng 4",
+      "Tháng 5",
+      "Tháng 6",
+      "Tháng 7",
+      "Tháng 8",
+      "Tháng 9",
+      "Tháng 10",
+      "Tháng 11",
+      "Tháng 12",
+    ];
+  }
+}
