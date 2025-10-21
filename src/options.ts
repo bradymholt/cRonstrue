3860edaf33ff53f5d2251729896e0d0b9c9d6617
Ethan Kuo <90064267+kuo-ethan@users.noreply.github.com>
2025-10-21 05:56:00 -0700

    feat: add option to connect specified day of month and day of week fields with 'only on' wording rather than 'and on' (#362)
    Co-authored-by: Ethan Kuo <ethan.kuo@tanium.com>

--- src/options.ts
+++ src/options.ts
@@ -5,6 +5,13 @@ export interface Options {
   monthStartIndexZero?: boolean;
   use24HourTimeFormat?: boolean;
   locale?: string | null;
+  /**
+   * The day of month and day of week are logical AND'ed (as opposed to OR'ed).
+   * For example, 0 5 11-17 * 5 is stringified to:
+   * "At 12:00 AM, on day 1, 2, and 3 of the month, only on Wednesday and Friday" rather than
+   * "At 12:00 AM, on day 1, 2, and 3 of the month, and on Wednesday and Friday"
+   */
+  logicalAndDayFields?: boolean;
 
   /**
    * @deprecated (DEPRECATED DO NOT USE) Timezone offset in minutes.
